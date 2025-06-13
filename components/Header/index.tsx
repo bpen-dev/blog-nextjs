929515ab9a5aae4ac07aa62efd3670c8af9fdf08
bpen-dev <214372532+bpen-dev@users.noreply.github.com>
2025-06-13 12:32:44 +0900

    headerのロゴを変更し、ブログタイトルを追記。レスポンシブ対応

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,20 +1,12 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import styles from './index.module.css';
+import Link from 'next/link'
+import styles from './index.module.css'
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link href="/">
-        <Image
-          src="/logo.svg"
-          alt="SIMPLE"
-          className={styles.logo}
-          width={348}
-          height={133}
-          priority
-        />
+      <Link href="/" className={styles.logoText}>
+        <h1 className={styles.logoTitle}>理系大学院生のぼちぼちITノート</h1>
       </Link>
     </header>
-  );
+  )
 }
