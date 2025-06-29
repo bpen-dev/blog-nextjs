332ae349ecd07cbf7f907b367fc70d5d6ee9602d
bpen-dev <214372532+bpen-dev@users.noreply.github.com>
2025-06-29 13:23:38 +0900

    免責、プライバシーポリシーページの追加

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,8 +1,19 @@
 import styles from './index.module.css';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
+      {/* ↓↓↓ ここから追加 ↓↓↓ */}
+      <nav className={styles.nav}>
+        <Link href="/disclaimer" className={styles.link}>
+          免責事項
+        </Link>
+        <Link href="/privacy-policy" className={styles.link}>
+          プライバシーポリシー
+        </Link>
+      </nav>
+      {/* ↑↑↑ ここまで追加 ↑↑↑ */}
       <p className={styles.cr}>© SIMPLE. All Rights Reserved 2023</p>
     </footer>
   );
