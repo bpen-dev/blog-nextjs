0eca20b64431844a57eb90bb3d1637fb705dc9f7
bpen-dev <214372532+bpen-dev@users.noreply.github.com>
2025-06-29 18:03:36 +0900

    フッターの調整

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -5,19 +5,21 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <nav className={styles.nav}>
+        {/* ↓↓↓ 「運営者情報」のリンクを追加 ↓↓↓ */}
+        <Link href="/writer" className={styles.link}>
+          運営者情報
+        </Link>
+        <Link href="/contact" className={styles.link}>
+          お問い合わせ
+        </Link>
         <Link href="/disclaimer" className={styles.link}>
           免責事項
         </Link>
         <Link href="/privacy-policy" className={styles.link}>
           プライバシーポリシー
         </Link>
-        {/* ↓↓↓ ここから追加 ↓↓↓ */}
-        <Link href="/contact" className={styles.link}>
-          お問い合わせ
-        </Link>
-        {/* ↑↑↑ ここまで追加 ↑↑↑ */}
       </nav>
-      <p className={styles.cr}>© 理系大学院生のぼちぼちITノート. All Rights Reserved 2025.</p>
+      <p className={styles.cr}>© 理系大学院生のぼちぼちITノート All Rights Reserved.</p>
     </footer>
   );
 }
